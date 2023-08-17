5b344ff15b44d0398274bcb7a562da83f8d6258b
HJ <eterion1@naver.com>
2023-08-17 11:59:42 +0900

    (int)
    23.08.17
    
    8비트의 double을 int로 변환하는 문제
    (int)로 간단하게 형변환하면 될 것을 절삭하고 형변환했다.

--- programmers/src/main/java/com/example/programmers/level_0/부분문자열인지확인하기.java
+++ programmers/src/main/java/com/example/programmers/level_0/부분문자열인지확인하기.java
@@ -8,14 +8,10 @@ public class 부분문자열인지확인하기 {
 		int answer =0;
 		answer = my_string.contains(target) ? 1: 0;
 		System.out.println(answer);
-		
-		
 		/*
 		my_string.indexOf(target) > -1 ? 1 : 0;
 		indexof의 없으면 -1을 출력하고 있으면 인덱스를 출력하는 특성을 이용한 방법
 		
 		 */
-		
-	
 	}
 }
---
+++ programmers/src/main/java/com/example/programmers/level_0/정수부분.java
@@ -0,0 +1,17 @@
+package com.example.programmers.level_0;
+
+public class 정수부분 {
+
+	public static void main(String[] args) {
+		int answer=0;
+		double flo = 7.2;
+		answer = (int)Math.floor(flo);
+		System.out.println(answer);
+		
+		
+		/*
+		(int)flo
+		
+		 */
+	}
+}
